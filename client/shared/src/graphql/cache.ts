45c0b644b6cca07c537343a71306aa41b3b4419a
Valery Bugakov <skymk1@gmail.com>
2021-07-30 16:37:08 +0300

    web: test `typePolicies`

--- client/shared/src/graphql/cache.ts
+++ client/shared/src/graphql/cache.ts
@@ -2,9 +2,17 @@ import { InMemoryCache } from '@apollo/client'
 
 import { TypedTypePolicies } from '../graphql-operations'
 
+import { IExtensionRegistry } from './schema'
+
 // Defines how the Apollo cache interacts with our GraphQL schema.
 // See https://www.apollographql.com/docs/react/caching/cache-configuration/#typepolicy-fields
-const typePolicies: TypedTypePolicies = {}
+const typePolicies: TypedTypePolicies = {
+    ExtensionRegistry: {
+        merge(existing: IExtensionRegistry, incoming: IExtensionRegistry): IExtensionRegistry {
+            return incoming
+        },
+    },
+}
 
 export const cache = new InMemoryCache({
     typePolicies,
